c8e1e7e474333edfd70184e77b2edb5323962312
Gregoire Chomette <greg.chomette@gmail.com>
2025-02-03 06:43:03 +0100

    tabs/resume.py: Simplified the code

--- tabs/resume.py
+++ tabs/resume.py
@@ -53,56 +53,21 @@ def resume(df_base, magasin_principal, magasins_comparatifs):
 
         st.markdown("<h4 style='text-align: center;'>Ma situation géographique</h4>", unsafe_allow_html=True)
         st.text("")
-        # folium_only_comparatifs = st.toggle('Afficher uniquement mon magasin et les magasins comparatifs', False)
-        folium_only_comparatifs = False
 
         fig_map_conso = folium.Map(location=[df_base_magasin['latitude'].mean(), df_base_magasin['longitude'].mean()], zoom_start=8)
         code = df_base_magasin['code'].values[0]
 
-        # Loop over all the magasins (df_base)
-        if folium_only_comparatifs == False:
-
-            for _, row in df_base.iterrows():
-
-                # Determine color based on 'conso_elec_mwh_par_m2' value
-                normalized_value = (row[var_couleur] - vmin) / (vmax - vmin) 
-
-                if row['code'] != code:
-                    # Add Circle marker (all magasins but the VIP one)
-                    folium.Circle(
-                        location=[row['latitude'], row['longitude']], radius=row['surface_com_m2'], 
-                        color="grey", weight=1, fill_color=mcolors.rgb2hex(plt.get_cmap('seismic')(normalized_value)), fill_opacity=0.5,
-                        tooltip=f"Num magasin: {row['num_magasin']}<br>Nom du manager: {row['manager']}<br>Surface commerciale: {int(row['surface_com_m2'])} m²<br>Consommation énergétique normalisée corrigée: {round(row[var_couleur],2)} MWh/m²",
-                    ).add_to(fig_map_conso)
-
-        # Loop over the magasins comparatifs only (df_magasins_comparatifs)
-        else:
-            df_magasins_comparatifs = df_base[df_base['num_magasin'].isin(magasins_comparatifs)]
-            for _, row in df_magasins_comparatifs.iterrows():
-
-                # Determine color based on 'conso_elec_mwh_par_m2' value
-                normalized_value = (row[var_couleur] - vmin) / (vmax - vmin)  
-
-                if row['code'] != code:
-                    # Add Circle marker (all magasins but the VIP one)
-                    folium.Circle(
-                        location=[row['latitude'], row['longitude']], radius=row['surface_com_m2'], 
-                        color="grey", weight=1, fill_color=mcolors.rgb2hex(plt.get_cmap('seismic')(normalized_value)), fill_opacity=0.5,
-                        tooltip=f"Num magasin: {row['num_magasin']}<br>Nom du manager: {row['manager']}<br>Surface commerciale: {int(row['surface_com_m2'])} m²<br>Consommation énergétique normalisée corrigée: {round(row[var_couleur],2)} MWh/m²",
-                    ).add_to(fig_map_conso)
-
-
         for _, row in df_base.iterrows():
 
-            if row['code'] == code:
+            # Determine color based on 'conso_elec_mwh_par_m2' value
+            normalized_value = (row[var_couleur] - vmin) / (vmax - vmin) 
 
-                # Determine color based on 'conso_elec_mwh_par_m2' value
-                normalized_value = (row[var_couleur] - vmin) / (vmax - vmin) 
-                folium.Circle(
-                        location=[row['latitude'], row['longitude']], radius=row['surface_com_m2'], 
-                        color="grey", weight=1, fill_color=mcolors.rgb2hex(plt.get_cmap('seismic')(normalized_value)), fill_opacity=0.5,
-                        tooltip=f"Num magasin: {row['num_magasin']}<br>Nom du manager: {row['manager']}<br>Surface commerciale: {int(row['surface_com_m2'])} m²<br>Consommation énergétique normalisée corrigée: {round(row[var_couleur],2)} MWh/m²",
-                    ).add_to(fig_map_conso)
+            # Add Circle marker (all magasins but the VIP one)
+            folium.Circle(
+                location=[row['latitude'], row['longitude']], radius=row['surface_com_m2'], 
+                color="grey", weight=1, fill_color=mcolors.rgb2hex(plt.get_cmap('seismic')(normalized_value)), fill_opacity=0.5,
+                tooltip=f"Num magasin: {row['num_magasin']}<br>Nom du manager: {row['manager']}<br>Surface commerciale: {int(row['surface_com_m2'])} m²<br>Consommation énergétique normalisée corrigée: {round(row[var_couleur],2)} MWh/m²",
+            ).add_to(fig_map_conso)
 
         
         st_folium(fig_map_conso, height=400, width=720)
